8024bf951581b0bd1ff5821c4c4455e75023918a
Fede <federremu@gmail.com>
2021-09-05 22:48:46 -0300

    crossOrigin all

--- src/main/java/com/vpi/springboot/Controller/PedidosController.java
+++ src/main/java/com/vpi/springboot/Controller/PedidosController.java
@@ -8,6 +8,7 @@ import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.vpi.springboot.exception.PedidosException;
 import com.vpi.springboot.mongoRepository.MongoRepo;
 import com.vpi.springboot.mongoServices.PedidosService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class PedidosController {
 
--- src/main/java/com/vpi/springboot/Controller/UserController.java
+++ src/main/java/com/vpi/springboot/Controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.vpi.springboot.Modelo.Usuario;
 import com.vpi.springboot.Repository.UserRepository;
 import com.vpi.springboot.exception.ResourceNotFoundException;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController 
 @RequestMapping("api/pruebas/")
 public class UserController {
