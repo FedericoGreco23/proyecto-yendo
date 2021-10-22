593d328627079019341784b621ecbe1965b255e0
camilo.suarez <camilo.suarez@fing.edu.uy>
2021-10-21 21:15:55 -0300

    listar promociones

--- src/main/java/com/vpi/springboot/Modelo/dto/DTPromocion.java
+++ src/main/java/com/vpi/springboot/Modelo/dto/DTPromocion.java
@@ -2,7 +2,9 @@ package com.vpi.springboot.Modelo.dto;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.vpi.springboot.Modelo.Producto;
 import com.vpi.springboot.Modelo.Promocion;
@@ -21,8 +23,21 @@ public class DTPromocion extends DTProducto implements Serializable {
 		super(promocion.getId(), promocion.getNombre(), promocion.getDescripcion(), promocion.getPrecio(),
 				promocion.getFoto(), promocion.getDescuento(), promocion.isActivo(), promocion.getRestaurante().getNombre());
 		
+		Map<String, Integer> duplicateMap = new HashMap<>();
 		for (Producto p : promocion.getProductos()) {
-			this.productos.add(p.getNombre());
+			if(!this.productos.contains(p.getNombre()))
+				this.productos.add(p.getNombre());
+			else {
+				if(duplicateMap.containsKey(p.getNombre())) {
+					duplicateMap.put(p.getNombre(), duplicateMap.get(p.getNombre()) + 1);
+				} else {
+					duplicateMap.put(p.getNombre(), 2);
+				}
+				
+				String nombreProducto = p.getNombre() + " x" + duplicateMap.get(p.getNombre());
+				this.productos.remove(p.getNombre());
+				this.productos.add(nombreProducto);
+			}
 		}
 	}
 
