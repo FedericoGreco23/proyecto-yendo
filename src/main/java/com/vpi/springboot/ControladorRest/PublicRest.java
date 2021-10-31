cb153f2baa3f7859370ca5b7ecda860d605ef8da
camilo.suarez <camilo.suarez@fing.edu.uy>
2021-10-31 19:49:52 -0300

    arreglo a alta cliente

--- src/main/java/com/vpi/springboot/ControladorRest/PublicRest.java
+++ src/main/java/com/vpi/springboot/ControladorRest/PublicRest.java
@@ -99,7 +99,7 @@ public class PublicRest {
 		try {
 			return new ResponseEntity<>(clienteService.altaCliente(usuario), HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>(new DTRespuesta(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
@@ -151,12 +151,12 @@ public class PublicRest {
 	}
 
 	@GetMapping("getRestaurante")
-	public ResponseEntity<DTRestaurante> getRestaurante(@RequestParam(defaultValue = "") String mail) {
+	public ResponseEntity<?> getRestaurante(@RequestParam(defaultValue = "") String mail) {
 		try {
 			DTRestaurante respuesta = service.getRestaurante(mail);
-			return new ResponseEntity<DTRestaurante>(respuesta, HttpStatus.OK);
+			return new ResponseEntity<>(respuesta, HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<DTRestaurante>(HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
