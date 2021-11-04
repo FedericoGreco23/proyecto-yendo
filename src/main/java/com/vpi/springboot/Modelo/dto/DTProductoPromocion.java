37c2bd4522d28acdf05591283cc05f0634ff8b3c
camilo.suarez <camilo.suarez@fing.edu.uy>
2021-11-04 18:20:57 -0300

    cambios a dtproducto

---
+++ src/main/java/com/vpi/springboot/Modelo/dto/DTProductoPromocion.java
@@ -0,0 +1,70 @@
+package com.vpi.springboot.Modelo.dto;
+
+import java.io.Serializable;
+
+import com.vpi.springboot.Modelo.Producto;
+
+public class DTProductoPromocion implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nombre;
+	private Integer cantidad;
+	private String categoria;
+
+	public DTProductoPromocion() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DTProductoPromocion(Producto producto) {
+		super();
+		this.id = producto.getId();
+		this.nombre = producto.getNombre();
+		this.categoria = producto.getCategoria().getNombre();
+		this.cantidad = 0;
+	}
+	
+	public DTProductoPromocion(Producto producto, int cantidad) {
+		super();
+		this.id = producto.getId();
+		this.nombre = producto.getNombre();
+		this.categoria = producto.getCategoria().getNombre();
+		this.cantidad = cantidad;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public void addCantidad() {
+		this.cantidad++;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+}
--- src/main/java/com/vpi/springboot/Modelo/dto/DTPromocion.java
+++ src/main/java/com/vpi/springboot/Modelo/dto/DTPromocion.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.vpi.springboot.Modelo.Producto;
 import com.vpi.springboot.Modelo.Promocion;
@@ -13,7 +14,7 @@ public class DTPromocion extends DTProducto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	// No tiene mucho sentido poner el DT aca
-	private List<String> productos = new ArrayList<>();
+	private List<DTProductoPromocion> productos = new ArrayList<>();
 
 	public DTPromocion() {
 		super();
@@ -23,45 +24,56 @@ public class DTPromocion extends DTProducto implements Serializable {
 		super(promocion.getId(), promocion.getNombre(), promocion.getDescripcion(), promocion.getPrecio(),
 				promocion.getFoto(), promocion.getDescuento(), promocion.isActivo(),
 				promocion.getRestaurante().getNombre());
+		
+		
 
-		Map<String, Integer> items = new HashMap<>();
+		Map<Integer, DTProductoPromocion> items = new HashMap<>();
 		for (Producto p : promocion.getProductos()) {
-			if (items.containsKey(p.getNombre())) {
-				items.put(p.getNombre(), items.get(p.getNombre()) + 1);
+			System.out.println("PRODUCTO: " + p.getNombre() + " - "  + p.getId());
+			if (items.containsKey(p.getId())) {
+				items.get(p.getId()).addCantidad();
 			} else {
-				items.put(p.getNombre(), 1);
+				items.put(p.getId(), new DTProductoPromocion(p));
 			}
-
-			String nombreProducto;
-			String nombreAnterior = "";
-			if (items.get(p.getNombre()) == 1) {
-				nombreProducto = p.getNombre();
-			} else if (items.get(p.getNombre()) == 2) {
-				nombreAnterior = p.getNombre() + " x" + items.get(p.getNombre());
-				nombreProducto = p.getNombre();
-			} else {
-				nombreAnterior = p.getNombre() + " x" + items.get(p.getNombre());
-				nombreProducto = p.getNombre() + " x" + (items.get(p.getNombre()) - 1);
-			}
-
-			if (!this.productos.contains(nombreProducto)) {
-				this.productos.add(nombreProducto);
-			} else {
-				this.productos.remove(nombreProducto);
-				this.productos.add(nombreAnterior);
-			}
+//
+//			String nombreProducto;
+//			String nombreAnterior = "";
+//			if (items.get(p.getNombre()) == 1) {
+//				nombreProducto = p.getNombre();
+//			} else if (items.get(p.getNombre()) == 2) {
+//				nombreAnterior = p.getNombre() + " x" + items.get(p.getNombre());
+//				nombreProducto = p.getNombre();
+//			} else {
+//				nombreAnterior = p.getNombre() + " x" + items.get(p.getNombre());
+//				nombreProducto = p.getNombre() + " x" + (items.get(p.getNombre()) - 1);
+//			}
+//
+//			if (!this.productos.contains(nombreProducto)) {
+//				this.productos.add(nombreProducto);
+//			} else {
+//				this.productos.remove(nombreProducto);
+//				this.productos.add(nombreAnterior);
+//			}
 		}
+		
+		List<DTProductoPromocion> retorno = new ArrayList<>();
+		retorno = items.entrySet().stream().map(e -> (e.getValue()))
+				.collect(Collectors.toList());
+		System.out.println("Lista final: " + retorno);
+		
+		this.productos = items.entrySet().stream().map(e -> (e.getValue()))
+				.collect(Collectors.toList());
 	}
 
-	public DTPromocion(List<String> productos) {
+	public DTPromocion(List<DTProductoPromocion> productos) {
 		this.productos = productos;
 	}
 
-	public List<String> getProductos() {
+	public List<DTProductoPromocion> getProductos() {
 		return productos;
 	}
 
-	public void setProductos(List<String> productos) {
+	public void setProductos(List<DTProductoPromocion> productos) {
 		this.productos = productos;
 	}
 }
