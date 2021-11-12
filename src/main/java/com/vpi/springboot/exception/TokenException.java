e5dd3128b53a600456a9e5f6c965cfbe6181befb
camilo.suarez <camilo.suarez@fing.edu.uy>
2021-11-11 21:57:35 -0300

    cambio a mensajes de token

--- src/main/java/com/vpi/springboot/exception/TokenException.java
+++ src/main/java/com/vpi/springboot/exception/TokenException.java
@@ -1,22 +1,23 @@
 package com.vpi.springboot.exception;
 
 public class TokenException extends Exception {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	public TokenException(String mensaje) {
 		super(mensaje);
 	}
-	
+
 	public static String NotFoundException() {
-		return "Hubo un error activando su cuenta.";
+		return "Ha ocurrido un error activando su cuenta. Por favor comuníquese con nosotros para resolver el problema.";
 	}
-	
+
 	public static String MailError() {
-		return "No se pudo mandar mail.";
+		return "Ha ocurrido un error al mandar el mail. Por favor comuníquese con nosotros para resolver el problema.";
 	}
-	
+
 	public static String ExpiredToken() {
-		return "El token expiró. Se ha enviado un nuevo mail de verificación.";
+		return "Su link de activación ha expirado. Se ha enviado un nuevo mail de verificación. "
+				+ "En caso de seguir teniendo problemas, por favor comuníquese con nosotros. ";
 	}
 }
